5c82f99792dd59decdba74b7af1a10d7c0732dca
Torstein <ttendstad@norsonic.com>
2022-11-10 23:02:06 +0100

    Implement string utils to fix segmentation fault

--- src/string_parsing_utils.hpp
+++ src/string_parsing_utils.hpp
@@ -3,33 +3,53 @@
 
 std::string GetSubstrBetween(std::string s, std::string prefix, std::string postfix)
 {
+	return "";
 }
 
 std::vector<std::string> SplitString(std::string s, std::string delimiter)
 {
+	int last_delimiter_idx = 0;
+	std::vector<std::string> substrings;
+	int i = 0;
+	while (i < s.size())
+	{
+		if (s[i] == delimiter[0])
+		{
+			substrings.push_back(s.substr(last_delimiter_idx, i - last_delimiter_idx));
+			last_delimiter_idx = i + 1;
+			i++;
+		}
+		i++;
+	}
+	substrings.push_back(s.substr(last_delimiter_idx, s.size()));
+	return substrings;
 }
 
 template <typename T>
 std::string ToString(T x)
 {
+	return std::to_string(x);
 }
 template std::string ToString(int);
 template std::string ToString(float);
 template std::string ToString(bool);
 
-template <typename T>
-T FromString(std::string s)
+template <>
+std::string ToString(sf::Vector2f x)
 {
+	return "(" + ToString(x.x) + "," + ToString(x.y) + ")";
 }
 
 template <typename T>
 void FromString(T& x, std::string s)
 {
+	x = (T)stod(s);
 }
 template void FromString(int&, std::string);
 template void FromString(float&, std::string);
 template void FromString(bool&, std::string);
 
+template <>
 void FromString(sf::Vector2f& x, std::string s)
 {
 }
