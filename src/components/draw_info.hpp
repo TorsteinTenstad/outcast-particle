20f00d200bd67d16ebeac548a2ef09b987f6073f
TorsteinTenstad <99toten@gmail.com>
2022-11-12 16:54:26 +0100

    Add ChargeDependentDrawInfo and OrientationDependentDrawInfo

--- src/components/draw_info.hpp
+++ src/components/draw_info.hpp
@@ -7,3 +7,18 @@ public:
 	std::string image_path;
 	bool rotated = false;
 };
+
+class ChargeDependentDrawInfo
+{
+public:
+	std::string positive_charge_image_path;
+	std::string neutral_charge_image_path;
+	std::string negative_charge_image_path;
+};
+
+class OrientationDependentDrawInfo
+{
+public:
+	std::string horisontal_image_path;
+	std::string vertical_image_path;
+};
