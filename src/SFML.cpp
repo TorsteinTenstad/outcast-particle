0908d1f2a106fa2e5237b6a69969408d19e9cad7
Alexander-Wulfsberg <gregeraw@stud.ntnu.no>
2022-11-18 19:24:22 +0100

    Update 10 timesd per frame

--- src/SFML.cpp
+++ src/SFML.cpp
@@ -8,7 +8,7 @@ SFML::SFML()
 	float screenScalingFactor = platform.getScreenScalingFactor(globals.render_window.getSystemHandle());
 	// Use the screenScalingFactor
 	globals.render_window.create(sf::VideoMode(1920 * screenScalingFactor, 1080 * screenScalingFactor), "outcast-particle"); //, sf::Style::Fullscreen);
-	globals.render_window.setFramerateLimit(60);
+	//globals.render_window.setFramerateLimit(60);
 	platform.setIcon(globals.render_window.getSystemHandle());
 }
 
@@ -19,8 +19,12 @@ void SFML::RunWindow(std::function<void(float)> update_func)
 	{
 		globals.render_window.clear();
 		float dt = timer.GetElapsedSeconds();
-		//std::cout << 1 / dt << "\n";
-		update_func(dt);
+		std::cout << 1 / dt << "\n";
+		for (int i = 0; i < 10; i++)
+		{
+			update_func(dt / 10);
+		}
+
 		globals.render_window.display();
 	}
 }
