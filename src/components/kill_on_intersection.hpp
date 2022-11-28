72d1162945d359253cd9186c0fa569572b6587b4
Torstein <ttendstad@norsonic.com>
2022-11-28 16:45:42 +0100

    Add levels

--- src/components/kill_on_intersection.hpp
+++ src/components/kill_on_intersection.hpp
@@ -1,6 +1,14 @@
 #pragma once
 #include "PCH.hpp"
 
+enum KillOnIntersectionChannels
+{
+	KillPlayer,
+	CollectCoin
+};
+
 class KillOnIntersection
 {
+	std::map<KillOnIntersectionChannels, bool> kills;
+	std::map<KillOnIntersectionChannels, bool> dies;
 };
