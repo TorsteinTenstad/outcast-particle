63bd81d7623e892bf4451d6662349a2c6f47681b
TorsteinTenstad <99toten@gmail.com>
2022-11-28 13:11:42 +0100

    Separate texture generation by color

--- content/generate_particle_textures.py
+++ content/generate_particle_textures.py
@@ -2,9 +2,6 @@ import subprocess
 import math
 inkscape = 'C:\\Program Files\\Inkscape\\bin\\inkscape.exe'
 
-colors = ['#0b3800', '#1f4c0b', '#346116', '#497721', '#5f8d31', '#76a446', '#8dbb5c', '#a4d372', '#cce5b1',
-          '#f5f5f5', '#f9d4d2', '#feb2ac', '#f1958f', '#de7b77', '#ca615e', '#b44849', '#9b3137', '#821924', '#670010']
-
 pluss_rect = """<rect
        style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:0.0896775;stroke-opacity:0"
        id="rect4792"
@@ -19,18 +16,25 @@ pluss_rect = """<rect
 with open('particle.svg', 'r') as f:
     svg_str = f.read()
 
-for i in range(5):
-    strength = 5-i
-    for j in [-1, 1]:
-        outer_color = colors[-j*(i+1) - (1 if j == 1 else 0)]
-        inner_color = colors[-j*i - (1 if j == 1 else 0)]
-        target = f'textures\\particle_{j*strength}.png'
+red_colors = ['#690000', '#8d0001', '#b30210', '#d22325',
+              '#ee4039', '#fe6757', '#ff907b', '#ffb59c']
+green_colors = ['#002b00', '#004600', '#006400',
+                '#038308', '#2ea12d', '#52c04b', '#73e068', '#98ff89']
+blue_colors = ['#002658', '#004177', '#035d97',
+               '#237ab7', '#4d96d6', '#6fb3f5', '#92d4ff', '#b6f6ff']
+for color_name, colors in zip(['red', 'green', 'blue'], [red_colors, green_colors, blue_colors]):
+    for sign in ['-', '+']:
+        for i in range(5):
+            strength = 5-i
+            outer_color = colors[i]
+            inner_color = colors[i+1]
+            target = f'textures\\particle_{color_name}_{sign}{strength}.png'
 
-        modified_svg_str = svg_str.replace('fill:#969696', f'fill:{outer_color}').replace(
-            'fill:#646464', f'fill:{inner_color}')
+            modified_svg_str = svg_str.replace('fill:#969696', f'fill:{inner_color}').replace(
+                'fill:#646464', f'fill:{outer_color}')
 
-        if j == -1:
-            modified_svg_str = modified_svg_str.replace(pluss_rect, '')
+            if sign == '-':
+                modified_svg_str = modified_svg_str.replace(pluss_rect, '')
 
-        subprocess.run([inkscape, '--export-type=png', f'--export-filename={target}',
-                        f'--export-width={240}', f'--export-height={240}', '--pipe'], input=modified_svg_str.encode())
+            subprocess.run([inkscape, '--export-type=png', f'--export-filename={target}',
+                            f'--export-width={240}', f'--export-height={240}', '--pipe'], input=modified_svg_str.encode())
