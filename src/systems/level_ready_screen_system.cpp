b409b1ac49247961678d763f19b145babaaea45b
TorsteinTenstad <99toten@gmail.com>
2023-03-12 13:57:19 +0100

    Make "Press any button to start" prompt always start with full opacity

--- src/systems/level_ready_screen_system.cpp
+++ src/systems/level_ready_screen_system.cpp
@@ -15,16 +15,18 @@ void LevelReadyScreenSystem::Update(Level& level, float dt)
 
 	auto [entity_id, ready_screen] = level.GetSingletonIncludeID<ReadyScreen>([&level_size, &level_grid_size](ESCScene& level, int entity_id) {
 		level.AddComponent<DrawInfo>(entity_id)->image_path = "content\\textures\\transparent.png";
+		level.AddComponent<DrawPriority>(entity_id)->draw_priority = UI_BASE_DRAW_PRIORITY + 5;
+		level.AddComponent<Position>(entity_id)->position = level_size / 2.f;
+		level.AddComponent<WidthAndHeight>(entity_id)->width_and_height = sf::Vector2f(0.75 * level_size.x, (3.f / 18.f) * level_size.y);
+		level.AddComponent<FillColor>(entity_id);
 		Text* text = level.AddComponent<Text>(entity_id);
 		text->content = "Press any key to start";
 		text->size = 200 * float(level_grid_size.y) / 16.f;
 		text->outline_color = sf::Color::Black;
 		text->outline_thickness = 10 * float(level_grid_size.y) / 16.f;
-		level.AddComponent<DrawPriority>(entity_id)->draw_priority = UI_BASE_DRAW_PRIORITY + 5;
-		level.AddComponent<Position>(entity_id)->position = level_size / 2.f;
-		level.AddComponent<WidthAndHeight>(entity_id)->width_and_height = sf::Vector2f(0.75 * level_size.x, (3.f / 18.f) * level_size.y);
-		level.AddComponent<AnimatedOpacity>(entity_id)->animation_func = [](float t) { return sf::Uint8(255 * Smoothstep(0.5 * std::cos(PI * t) + 0.5)); };
-		level.AddComponent<FillColor>(entity_id);
+		AnimatedOpacity* animated_opacity = level.AddComponent<AnimatedOpacity>(entity_id);
+		animated_opacity->animation_func = [](float t) { return sf::Uint8(255 * Smoothstep(0.5 * std::cos(PI * t) + 0.5)); };
+		animated_opacity->start_time = globals.time;
 	});
 
 	auto conditionaly_enter_play_mode = [&level](bool enter) {
